4978ca795ebd630e4bc04e5380ff389f59b39c01
emnuelle <rm97973@fiap.com.br>
2023-04-18 11:43:41 -0300

    deu pau até no do professor

--- JS/zyb-5647.js
+++ JS/zyb-5647.js
@@ -1,12 +1,20 @@
-function soma(parametro) {
-    nr1 = 10;
-    nr2 = 5;
+// function soma(parametro) {
+//     nr1 = 10;
+//     nr2 = 5;
 
-    console.log(`Resultado : ${nr1 + nr2 + parametro}`);
+//     console.log(`Resultado : ${nr1 + nr2 + parametro}`);
 
-    console.log(`Este é o parâmetro que foi passado para a função : ${parametro}`);
+//     console.log(`Este é o parâmetro que foi passado para a função : ${parametro}`);
     
-    return (parseint(nr1) + parseint(nr2) + parseint(parametro));
+//     return (parseint(nr1) + parseint(nr2) + parseint(parametro));
+// }
+
+// soma();
+
+// arrow function
+const mudacor = ()=>{
+    // função set time out
+    setTimeout("EXECUTOU",5000);
 }
 
-soma();
+mudacor();
