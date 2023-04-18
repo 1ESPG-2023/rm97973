34c06f02d71db523b94acdbb2fe9ebe399065ea2
emnuelle <rm97973@fiap.com.br>
2023-04-18 11:33:32 -0300

    n entendi nada

--- JS/zyb-5647.js
+++ JS/zyb-5647.js
@@ -1,50 +1,12 @@
+function soma(parametro) {
+    nr1 = 10;
+    nr2 = 5;
 
-// const liElement = document.getElementsByTagName("li");
+    console.log(`Resultado : ${nr1 + nr2 + parametro}`);
 
-
-// // Forma intelegente de fazer a leitura de uma coleção
-// for (let index = 0; index < liElement.length; index++) {
-//     console.log(liElement[index].textContent)
+    console.log(`Este é o parâmetro que foi passado para a função : ${parametro}`);
     
-// }
+    return (parseint(nr1) + parseint(nr2) + parseint(parametro));
+}
 
-// // console.log(liElement.length)
-
-// Declaração de arrays
-let nr1 = [1,2,3,4,5,];
-let nr2 = [6,7,8,9,10];
-let nr3 = [...nr1,...nr2];
-
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
-
-// Listando o Array - tecnica especial
-// nr3.forEach((itemDoArray)=>{
-    
-// console.log(`Array-3 sendo listado ${nr3}`);    
-
-// })
-
-nr3.map((numero,key)=>{
-    console.log(`Itens do Array - ${(key+1) - numero}`);
-    // numero.forEach((digitos)=>{
-    //     console.log(digitos)
-    // });
-})
-
-// convertendo html collection em array de forma indireta
-const liElementHtml = document.getElementsByTagName("li");
-console.log(liElementHtml);
-
-const liElementArray = [...liElementHtml];
-console.log(liElementArray);
-
-liElementArray.map((item,key)=>{
-    if (item.textContent == "item-39") {
-        console.log(`${key+1} - ${item.textContent = "LOCALIZADO"} `)
-    }
-// o item 44 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-})
-
-
+soma();
