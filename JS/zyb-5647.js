aba522a4e82e86d85fb90cc5560dc715d06673eb
emnuelle <rm97973@fiap.com.br>
2023-05-02 10:45:25 -0300

    arrumando

--- JS/zyb-5647.js
+++ JS/zyb-5647.js
@@ -1,32 +1,18 @@
-// mudacor()
+// Função altera banner 
 
-function mudacor() {
+function alteraBanner() {
 
-    let r = "";
-    let g = "";
-    let b = "";
+    const img1 = document.querySelector(".l-e > img")
+    const img2 = document.querySelector(".l-d > img")
 
+    let nr = Math.ceil(Math.random() * 3);
+    let caminho = `../img/banner-lateral-${nr}.png`;
+    img1.src = caminho;
+
+    nr = Math.ceil(Math.random() * 3);
+    caminho = `../img/banner-lateral-${nr}.png`;
+    img2.src = caminho;
+
+    setTimeout(alteraBanner, 1000)
     
-    //random = gera números aleatórios entre 0 e 1
-    //ceil, floor e round que arrendondam o número para cima, baixo ou aleatóriamente
-    
-    //Determinando para a variavél r um valor entre 0 e 255
-    r = Math.round(Math.random() * 255); 
-    g = Math.round(Math.random() * 255);
-    b = Math.round(Math.random() * 255);
-    
-    const cabecalho = document.querySelector(".cabecalho");
-    const container1 = document.querySelector(".container .l-e");
-    const container2 = document.querySelector(".container .l-d");
-    const rodape = document.querySelector(".rodape");
-
-    cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
-    container1.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
-    container2.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
-    rodape.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
-
-    tmp = setTimeout(mudacor,100);
-
 }
-
-mudacor();
